f4fe498ddadf10326bded382b1310da9cc3be323
Saurabh Prajapati <saurabh945644@gmail.com>
2021-10-03 16:25:24 +0530

    added conditional rendering

--- src/componants/conversation.js
+++ src/componants/conversation.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Message from "./message";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 export default function Conversation(props) {
+  const [openCoversation, setOpenConversation] = useState(false);
+
+  const toggleOpen = () => {
+    setOpenConversation(!openCoversation);
+  };
   return (
     <div>
-      <div>{props.conversation["UUID"]}</div>
-      {props.conversation.messages.map((message) => (
-        <Message message={message} />
-      ))}
+      <Card onClick={toggleOpen}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {props.conversation["UUID"]}
+          </Typography>
+        </CardContent>
+      </Card>
+      {openCoversation &&
+        props.conversation.messages.map((message) => (
+          <Message message={message} />
+        ))}
     </div>
   );
 }
